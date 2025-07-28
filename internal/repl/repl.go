50f8276bed036e09a9c59e275ee6cd4ce82af385
yurikdotdev <172495341+yurikdotdev@users.noreply.github.com>
2025-07-28 19:19:25 +0700

    feat(repl): improve keyboard handling with liner

--- internal/repl/repl.go
+++ internal/repl/repl.go
@@ -1,12 +1,12 @@
 package repl
 
 import (
-	"bufio"
 	"fmt"
 	"io"
 	"os"
 	"strings"
 
+	"github.com/peterh/liner"
 	"github.com/yurikdotdev/covfefescript/internal/eval"
 	"github.com/yurikdotdev/covfefescript/internal/lexer"
 	"github.com/yurikdotdev/covfefescript/internal/object"
@@ -14,14 +14,16 @@ import (
 )
 
 func InitREPL() {
+	line := liner.NewLiner()
+	defer line.Close()
+
 	if len(os.Args) > 1 {
 		RunFile(os.Args[1])
 	} else {
 		fmt.Print(TRUMP_ASCII_INTRO)
 		fmt.Println("\nCovfefeScript 0.1 | The Best Interpreter, Believe Me.")
 		fmt.Print("Type 'CHYNA' to quit.\n\n")
-
-		StartREPL(os.Stdin, os.Stdout)
+		StartREPL(line, os.Stdout)
 	}
 }
 
@@ -48,36 +50,49 @@ func RunFile(filename string) {
 	env := object.NewEnvironment()
 	evaluated := eval.Eval(program, env)
 
-	if evaluated != nil && evaluated.Type() == object.ERROR_OBJ {
-		fmt.Fprintln(os.Stderr, evaluated.Inspect())
-		os.Exit(1)
+	// fmt.Fprintf(os.Stderr, "DEBUG: evaluated = %v, type = %s\n", evaluated, evaluated.Type())
+
+	if evaluated != nil {
+		if evaluated.Type() == object.ERROR_OBJ {
+			fmt.Fprintln(os.Stderr, evaluated.Inspect())
+			os.Exit(1)
+		}
 	}
 }
 
-func StartREPL(in io.Reader, out io.Writer) {
-	scanner := bufio.NewScanner(in)
+func StartREPL(line *liner.State, out io.Writer) {
 	env := object.NewEnvironment()
 
 	for {
-		fmt.Fprint(out, REPL_PROMPT_SIGN)
-		scanned := scanner.Scan()
-		if !scanned {
+		input, err := line.Prompt(REPL_PROMPT_SIGN)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Print(TRUMP_ASCII_OUTRO)
+			} else {
+				fmt.Printf("SAD! A terrible error reading input: %s\n", err)
+			}
 			return
 		}
 
-		line := scanner.Text()
-		if line == "CHYNA" {
+		if input == "" {
+			continue
+		}
+
+		line.AppendHistory(input)
+
+		if input == "CHYNA" {
 			fmt.Print(TRUMP_ASCII_OUTRO)
 			return
 		}
 
-		// he who shall not be named
+		// HE WHO SHALL NOT BE NAMED
 		target := string([]rune{101, 112, 115, 116, 101, 105, 110})
-		if strings.ToLower(line) == target {
+		if strings.ToLower(input) == target {
+			fmt.Print("Really bro?")
 			return
 		}
 
-		l := lexer.New(line)
+		l := lexer.New(input)
 		p := parser.New(l)
 		program := p.ParseProgram()
 
@@ -96,7 +111,7 @@ func StartREPL(in io.Reader, out io.Writer) {
 
 func printParserErrors(out io.Writer, errors []string) {
 	io.WriteString(out, TRUMP_ASCII_LOSER)
-	io.WriteString(out, "SAD! Unbelievable :\n")
+	io.WriteString(out, "SAD! Unbelievable, what a loser:\n")
 	for _, msg := range errors {
 		io.WriteString(out, ""+msg+"\n\n")
 	}
